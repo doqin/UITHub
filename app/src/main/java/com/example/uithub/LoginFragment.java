6b5b317ae14974f8301de0f2c2ecaa6812bfdb36
tuankhang <personal.azalea@gmail.com>
2026-03-19 20:29:24 +0700

    Added LoginFragment and BottomNavigationView

---
+++ app/src/main/java/com/example/uithub/LoginFragment.java
@@ -0,0 +1,9 @@
+package com.example.uithub;
+
+import androidx.fragment.app.Fragment;
+
+public class LoginFragment extends Fragment {
+    public LoginFragment() {
+        super(R.layout.fragment_login);
+    }
+}
